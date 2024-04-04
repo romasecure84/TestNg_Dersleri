f21320800a9eee4d99c41751cf29092abf1bea6e
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-04-04 22:18:30 +0400

    Hard Assert dersi elave edildi

---
+++ src/test/java/tests/Ders09/C05_HardAssert.java
@@ -0,0 +1,30 @@
+package tests.Ders09;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBaseBeforeMethodAfterMethod;
+
+public class C05_HardAssert extends TestBaseBeforeMethodAfterMethod {
+    @Test
+    public void test() throws InterruptedException {
+        //amazon sehifesine gedin
+        driver.get("https://www.amazon.com.tr");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id='sp-cc-rejectall-link']")).click();
+        //title'de Amazon yazildigini test edin
+        Assert.assertTrue(driver.getTitle().contains("Amazon"));
+        //axtaris qutusunun movcudlugunun test edin
+        WebElement axtarisQutusu=driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"));
+        Assert.assertTrue(axtarisQutusu.isEnabled());
+        //axtaris qutusuna "Iphone" yazib axtaris edin
+        axtarisQutusu.sendKeys("Iphone", Keys.ENTER);
+        //axtarisin edildiyini test edin
+        WebElement neticeler= driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
+        Assert.assertTrue(neticeler.isDisplayed());
+        //axtaris neticesinin iphone icerdiyini test edin
+        Assert.assertTrue(neticeler.getText().contains("Iphone"));
+    }
+}
