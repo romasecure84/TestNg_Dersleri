df30374d626465fa20de720ac2125ab8821b7202
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-04-04 22:31:02 +0400

    Soft Assert dersi elave edildi

---
+++ src/test/java/tests/Ders09/C06_SoftAssert.java
@@ -0,0 +1,34 @@
+package tests.Ders09;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import utilities.TestBaseBeforeMethodAfterMethod;
+
+public class C06_SoftAssert extends TestBaseBeforeMethodAfterMethod {
+    @Test
+    public void test() throws InterruptedException {
+        SoftAssert softAssert=new SoftAssert();
+        //amazon sehifesine gedin
+        driver.get("https://www.amazon.com.tr");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id='sp-cc-rejectall-link']")).click();
+        //title'de Amazon yazildigini test edin
+        softAssert.assertTrue(driver.getTitle().contains("amazon"),"Girdiyiniz kelime movcud deyil");
+        //axtaris qutusunun movcudlugunun test edin
+        WebElement axtarisQutusu=driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"));
+        softAssert.assertTrue(axtarisQutusu.isEnabled(),"Axtaris qutusu movcud deyil");
+        //axtaris qutusuna "Iphone" yazib axtaris edin
+        axtarisQutusu.sendKeys("Iphone", Keys.ENTER);
+        //axtarisin edildiyini test edin
+        WebElement neticeler= driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
+        softAssert.assertTrue(neticeler.isDisplayed(),"Neticeler movcud deyil");
+        //axtaris neticesinin iphone icerdiyini test edin
+        softAssert.assertTrue(neticeler.getText().contains("iphone"),"axtardiginiz kelime yoxdur");
+        softAssert.assertAll();
+        System.out.println("eger sistemde xeta varsa bu yazi oxunmayacaq");
+    }
+}
