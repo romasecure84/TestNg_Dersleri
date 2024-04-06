94d8b312922055028c920f34c0b51cf6962e2450
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-04-06 17:43:27 +0400

    Positive smike test dersi elave edildi

---
+++ src/test/java/tests/Ders10/C06_SmokePositiveTest.java
@@ -0,0 +1,22 @@
+package tests.Ders10;
+
+import org.testng.annotations.Test;
+import pages.BrcPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C06_SmokePositiveTest {
+    @Test
+    public void positiveTest() {
+        BrcPage brcPage=new BrcPage();
+        //https://www.bluerentalcars.com/ adresine gedin
+        Driver.getDriver().get(ConfigReader.getProperty("brcURL"));
+        //email, password ve login button'larini locate edin
+        brcPage.loginButton.click();
+        brcPage.email.sendKeys(ConfigReader.getProperty("brcUser"));
+        brcPage.password.sendKeys(ConfigReader.getProperty("brcPass"));
+        brcPage.loginButton2.click();
+        //Deyerleri girildiyinde sehifeye ugurlu bir sekilde girilebildiyinin test edin
+        //Assert.assertTrue ile kontrol edilir!
+    }
+}
